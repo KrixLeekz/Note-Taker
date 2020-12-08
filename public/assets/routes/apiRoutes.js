8ddffff0f477c5742e3593ed9c846816a8facaa7
Chris Lyko <chrislyko@gmail.com>
2020-12-08 11:54:51 -0500

    fixed routing error, added delete method

--- public/assets/routes/apiRoutes.js
+++ public/assets/routes/apiRoutes.js
@@ -1,13 +1,34 @@
-//require Notes
-
-const notesData = require('../../../db/db.json')
+//file systems
+const fs = require('fs') 
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => {
-        res.json(notesData)
-    })
+    fs.readFile('db/db.json', 'utf-8', (error, noteData) => {
 
-    app.post('/api/notes', (req,res) => {
-        notesData.push(req.body)
+        if (error) throw error
+
+        const data = JSON.parse(noteData)
+        
+        app.get('/api/notes', (req, res) => {
+            res.json(data)
+        })
+    
+        app.post('/api/notes', (req,res) => {
+            data.push(req.body)
+            fs.writeFile('db/db.json', JSON.stringify(data, '\t'), error => {
+                if (error) throw error
+            })
+            console.log("wrote file")
+        })
+
+        app.get('/api/notes/:id', (req,res) => {
+            res.json(data[req.params.id])
+        })
+
+        app.delete("/api/notes/:id", (req, res) => {
+            data.splice(req.params.id, 1);
+            fs.writeFile('db/db.json', JSON.stringify(data, '\t'), error => {
+                if (error) throw error
+            })
+        });
     })
 }
--- public/assets/routes/htmlRoutes.js
+++ public/assets/routes/htmlRoutes.js
@@ -1,17 +1,15 @@
 const path = require('path')
+const fs = require('fs')
 
 module.exports = (app) => {
-    //home page
-    app.get("/", (req,res) => {
-        res.sendFile(path.join(__dirname, "../../index.html"))
-    })
-    //notes page
-    app.get("/notes", (req,res) => {
-        res.sendFile(path.join(__dirname, "../../notes.html"))
-    })
-    //if no matching route found it defaults to home page
-    app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../../index.html"))
-        console.log(__dirname)
+    fs.readFile('db/db.json', 'utf-8', () => {
+        //notes page
+        app.get("/notes", (req,res) => {
+            res.sendFile(path.join(__dirname, "../../notes.html"))
+        })
+        //if no matching route found it defaults to home page
+        app.get("*", (req,res) => {
+            res.sendFile(path.join(__dirname, "../../index.html"))
+        })
     })
 }
