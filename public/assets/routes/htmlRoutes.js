8a43b6ebd070578f8f76753b536a6dca036da685
Chris Lyko <chrislyko@gmail.com>
2020-12-08 08:42:00 -0500

    added default

--- public/assets/routes/htmlRoutes.js
+++ public/assets/routes/htmlRoutes.js
@@ -1,12 +1,17 @@
 const path = require('path')
 
 module.exports = (app) => {
+    //home page
     app.get("/", (req,res) => {
         res.sendFile(path.join(__dirname, "../../index.html"))
-        console.log(__dirname)
     })
-
+    //notes page
     app.get("/notes", (req,res) => {
         res.sendFile(path.join(__dirname, "../../notes.html"))
+    })
+    //if no matching route found it defaults to home page
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../../index.html"))
+        console.log(__dirname)
     })
 }
